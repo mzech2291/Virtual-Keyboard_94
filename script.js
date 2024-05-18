92964b5abe9125d55e735985615937125a829a4d
Bhumika Chopra <160768873+bhumikachopra11@users.noreply.github.com>
2024-05-18 13:14:40 +0530

    Update script.js
    Toggled night mode styles.

--- script.js
+++ script.js
@@ -61,4 +61,17 @@ window.addEventListener('keyup', function(e) {
     }
 })
 
+night_mode.addEventListener('click',function() {
+    toggle_circle.classList.toggle('active');
+    body.classList.toggle('active');
+    night_mode.classList.toggle('active');
+    keyboard_wrapp.classList.toggle('active');
+    text_input.classList.toggle('active');
+    change_color.classList.toggle('active');
+    for(let i = 0; i < keys.length; i++) {
+        keys[i].classList.toggle('keys_night')
+    }
+})
+
+
 
