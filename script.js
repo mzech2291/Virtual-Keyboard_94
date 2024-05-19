f41d86150d4f12928ab6e23b1b79f9ef7e79d324
Bhumika Chopra <160768873+bhumikachopra11@users.noreply.github.com>
2024-05-19 15:57:23 +0530

    Update script.js
    code completion

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ for(let i = 0; i < keys.length; i++) {
     keys[i].setAttribute('lowerCaseName', keys[i].innerText.toLowerCase());
 }
 
-
 window.addEventListener('keydown', function(e) {
     for(let i = 0; i < keys.length; i++) {
         if(e.key == keys[i].getAttribute('keyname' ) || e.key == keys[i].getAttribute('lowerCaseName')) {
