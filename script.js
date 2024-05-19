48423455a67c5b02b92382be1e6a810ba02ef750
Bhumika Chopra <160768873+bhumikachopra11@users.noreply.github.com>
2024-05-19 09:41:38 +0530

    Update script.js
    Updated keyboard colors dynamically.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ for(let i = 0; i < keys.length; i++) {
     keys[i].setAttribute('lowerCaseName', keys[i].innerText.toLowerCase());
 }
 
+
 window.addEventListener('keydown', function(e) {
     for(let i = 0; i < keys.length; i++) {
         if(e.key == keys[i].getAttribute('keyname' ) || e.key == keys[i].getAttribute('lowerCaseName')) {
@@ -71,6 +72,13 @@ night_mode.addEventListener('click',function() {
     for(let i = 0; i < keys.length; i++) {
         keys[i].classList.toggle('keys_night')
     }
+})
+
+colors_input.addEventListener('input',function() {
+    for(let i = 0; i < keys.length; i++) {
+        keys[i].style.color = colors_input.value
+    }
+    keyboard_lights.style.background = colors_input.value;
 })
 
 
